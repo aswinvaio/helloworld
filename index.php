6081b0737161b954dda0bba5450cf6648ac32250
aswin vp <aswinprabhakaran@gmail.com>
2016-10-10 21:59:40 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aswin
+ * Date: 10-10-2016
+ * Time: 08:18 PM
+ */
+
+?>
+<html>
+<head>
+    <title>Hello world</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="span12">
+            <form class="form-horizontal" action='' method="POST">
+                <fieldset>
+                    <div id="legend">
+                        <legend class="">Login</legend>
+                    </div>
+                    <div class="control-group">
+                        <!-- Username -->
+                        <label class="control-label"  for="username">Username</label>
+                        <div class="controls">
+                            <input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <!-- Password-->
+                        <label class="control-label" for="password">Password</label>
+                        <div class="controls">
+                            <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <!-- Button -->
+                        <div class="controls">
+                            <button class="btn btn-success">Login</button>
+                            <button class="btn btn-success">Sign up</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<script src="http://code.jquery.com/jquery.js"></script>
+</body>
+</html>
